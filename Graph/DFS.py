229cfdf27f168fd056ac4a0ec4cd98be67fc8c03
Mouzan Raza <mouzan.ai.dev@gmail.com>
2026-02-19 19:06:38 +0500

    Graph traversel method

---
+++ Graph/DFS.py
@@ -0,0 +1,33 @@
+class graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def dfs(self, start, visited=None):
+        if visited is None:
+            visited = set()
+        visited.add(start)
+        print(start,end="->")  # Process the node (e.g., print it)
+        for neighbor in self.graph.get(start, []):
+            if neighbor not in visited:
+                self.dfs(neighbor, visited)
+
+
+if __name__ == "__main__":
+    g = graph()
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 3)
+    g.add_edge(1, 4)
+    g.add_edge(2, 5)
+    g.add_edge(2, 6)
+
+    print("DFS starting from node 0:")
+    g.dfs(0)

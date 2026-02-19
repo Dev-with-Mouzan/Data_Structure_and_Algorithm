0b9144313080fca321cca7a833fcb220e08ea9b2
Mouzan Raza <mouzan.ai.dev@gmail.com>
2026-02-19 19:05:52 +0500

    List representation

---
+++ Graph/Adjcent_List_representation.py
@@ -0,0 +1,20 @@
+class graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = {i: [] for i in range(vertices)}
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def print_Graph(self):
+        for key in self.graph:
+            print(f"{key} -> {self.graph[key]}")
+
+if __name__ == "__main__":
+    g = graph(5)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 3)
+    g.add_edge(1, 4)
+    g.print_Graph()

2bfa226ee40e33fb0154dcc3b50b21db7902f7da
Mouzan Raza <mouzan.ai.dev@gmail.com>
2026-03-01 20:33:45 +0500

    Update

--- Insertion_sort/intersertion_sort.py
+++ Insertion_sort/intersertion_sort.py
@@ -11,4 +11,9 @@ def insertion_sort(arr):
 
 if __name__ == "__main__":
     arr = [12, 11, 13, 5, 6]
+    arr1 = [23,45,67,89,4,2]
+    #print first array
     print(insertion_sort(arr))
+
+    # print second array
+    print(insertion_sort(arr1))
